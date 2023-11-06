a046a18dab14dd5e81c06af26002522fc98cae94
alvccpj <dev.alvarojordao@gmail.com>
2023-11-06 10:50:46 -0300

    add Main and ConsultaCep changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner in = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
-        Endereco novoEndereco = consultaCep.buscaEndereco("50710050");
-        System.out.printf(String.valueOf(novoEndereco));
+        System.out.print("Informe o CEP para consulta: ");
+        String cep = in.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.printf(String.valueOf(novoEndereco));
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicacação...");
+        }
+
     }
 }
